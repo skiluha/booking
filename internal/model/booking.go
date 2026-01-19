f16c045f9dd2023932ffa674eac7f73809b6ff50
skiluha <33bozhenka33@gmail.com>
2026-01-19 21:14:04 +0400

    add models

--- internal/model/booking.go
+++ internal/model/booking.go
@@ -1,1 +1,11 @@
 package model
+
+import "time"
+
+type Booking struct {
+	ID        int       `json:"id"`
+	RoomID    int       `json:"room_id"`
+	StartTime time.Time `json:"start_time"`
+	EndTime   time.Time `json:"end_time"`
+	CreatedAt time.Time `json:"created_at"`
+}
--- internal/model/room.go
+++ internal/model/room.go
@@ -1,1 +1,10 @@
 package model
+
+import "time"
+
+type Room struct {
+	ID        int       `json:"id"`
+	Name      string    `json:"name"`
+	Capacity  int       `json:"capacity"`
+	CreatedAt time.Time `json:"created_at"`
+}
